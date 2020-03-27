063b80128e2783e9af81c2315ae6f167d7d7d397
Victor Batigin <bixs85@gmail.com>
2020-03-27 23:56:43 +0200

    fix menu function

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,7 +3,18 @@ import menuTemplate from '../templates/menu.hbs';
 
 const menuListRef = document.querySelector('.js-menu');
 
-menu.forEach(element => {
-  const markup = menuTemplate(element);
-  menuListRef.insertAdjacentHTML('beforeend', markup);
-});
+// menu.forEach(element => {
+//   const markup = menuTemplate(element);
+//   menuListRef.insertAdjacentHTML('beforeend', markup);
+// });
+
+const addListTag = arr => {
+  const item = arr.reduce((acc, element) => {
+    const markup = menuTemplate(element);
+    acc += markup;
+    return acc;
+  }, '');
+  menuListRef.insertAdjacentHTML('beforeend', item);
+};
+
+addListTag(menu);
